29eb23bc11bf137e94c49a58f0ac9deafc768fc7
Saurabh Mangalekar <saurabhmangalekar@gmail.com>
2021-06-10 11:35:36 +0530

    Title Page created

---
+++ src/pages/Starred.js
@@ -0,0 +1,8 @@
+import React from "react";
+import MainPageLayout from "../components/MainPageLayout";
+
+const Starred = () => {
+  return <MainPageLayout>This is Starred page</MainPageLayout>;
+};
+
+export default Starred;
